8483bd8fe3ca567fd3e7181e9f7f347f2bff40a1
robinNcode <msmrobin518@gmail.com>
2025-03-19 07:33:41 +0600

    update: Mongoose cloud connection added.

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
+const connectionURI = process.env.MONGO_URI;
+const MONGO_PRODUCTION_URI_PASS = ":R@w].h.>N)9'K7";
+const password = encodeURIComponent(MONGO_PRODUCTION_URI_PASS);
+const prodConnectionURI = `mongodb+srv://robinNcode:${password}@robinncodecluster.ylh4h.mongodb.net/?retryWrites=true&w=majority&appName=robinncodeCluster`;
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(prodConnectionURI); // Removed deprecated options
+        console.log("MongoDB Connected Successfully!");
     } catch (err) {
         console.error("MongoDB Connection Error: ", err.message);
         process.exit(1);
